7c556651cbeb8620d82175462996187e10081c74
Dean Howe <hideanhowe@gmail.com>
2025-12-16 09:02:33 +0000

    loop solution

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 // Sum of Squares
 // Given a positive integer up to 1,000, return the sum of all the integers squared from 1 up to the number.
+
+function sumOfSquares(n) {
+  let counter = 0;
+  // console.log(n)
+  if (n <= 1000) {
+    for (let i = 1; i <= n; i++) {
+      console.log(i);
+      counter = counter + i * i;
+    }
+  }
+
+  return counter;
+}
